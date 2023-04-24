82b8945d34ee4555d4da4d078878fdfd475f99ca
Eugene Snopok <eugenesnopok@gmail.com>
2023-04-24 13:53:46 +0300

    remove redundant comments

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -27,13 +27,7 @@ async def create_user(user: UserCreateModel, db_session=Depends(get_db)):
         Pydantic model with neccessary field
 
     """
-    # validate_user_is_not_exist(cursor, user.email)
-    # user_id = insert_user(db, user)
-    # try:
     new_user = User(**user.dict())
     new_user.password = Hasher.get_password_hash(new_user.password)
-    # try:
     await new_user.save(db_session)
-    # except IntegrityError as no_unique_key:
-    #     print("")
     return new_user
