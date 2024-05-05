81932a736ebac0c34d9b2966d7105f6420af5148
kuhhaptc <eugenesnopok@gmail.com>
2024-05-05 13:38:44 +0300

    fix old routers

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,7 +1,9 @@
 from .auth import auth_router
+from .movie import movie_router
 from .user import user_router
 
 __all__ = [
     "auth_router",
     "user_router",
+    "movie_router",
 ]
--- app/routers/user.py
+++ app/routers/user.py
@@ -1,26 +1,53 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, Header, status
 
-from app.core.jwt import get_current_user
-from app.schemas.user import UserResponseModel
+from app.dependencies import get_user_service
+from app.schemas import UserResponseSchema, UserReviewSchema, UserSchema
+from app.services import UserService
 
 user_router = APIRouter(tags=["users"])
 
 
-@user_router.get("/me", summary="User page.", status_code=status.HTTP_200_OK)  # , response_model=UserResponseModel
-async def retrieve_current_user(user: UserResponseModel = Depends(get_current_user)) -> UserResponseModel:
-    """Retrieve user.
+@user_router.get("/me", summary="User page.", status_code=status.HTTP_200_OK, response_model=UserResponseSchema)
+async def retrieve_current_user(
+    authorization: str = Header(...), user_service: UserService = Depends(get_user_service)
+) -> UserSchema:
+    """Retrieve current user.
 
     Parameters
     ----------
-    user: UserCreateModel
-        Pydantic model for user creation
-    db_session: AsyncSession
-        Asynchronous connection to database
+    authorization: str
+        Header 'authorization' with jwt token
+    user_service: UserService
+        Service for user operations
 
     Returns
     -------
-    new_user: UserResponseModel
-        Pydantic model with neccessary field
+    user: UserResponseSchema
+        Pydantic model of user
 
     """
-    return UserResponseModel(**user.dict())
+    user = await user_service.get_current_user(authorization)
+    return user
+
+
+@user_router.get("/me/movies", summary="User page.", status_code=status.HTTP_200_OK, response_model=UserReviewSchema)
+async def get_user_with_movies(
+    authorization: str = Header(...), user_service: UserService = Depends(get_user_service)
+) -> UserReviewSchema:
+    """Retrieve current user with movies he watched.
+
+    Parameters
+    ----------
+    authorization: str
+        Header 'authorization' with jwt token
+    user_service: UserService
+        Service for user operations
+
+    Returns
+    -------
+    user: UserReviewSchema
+        Pydantic model of user with his movies
+
+    """
+    user = await user_service.get_current_user_with_movies(authorization)
+    return user
